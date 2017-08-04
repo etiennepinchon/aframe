e06c29e28c92a12a1cf14a21c1382d8100688d15
Etienne Pinchon <etiennekingdom@gmail.com>
2017-08-04 23:09:57 +0200

    Adding shadows to a-light

--- src/extras/primitives/primitives/a-light.js
+++ src/extras/primitives/primitives/a-light.js
@@ -5,15 +5,27 @@ registerPrimitive('a-light', {
     light: {}
   },
 
-  mappings: {
-    angle: 'light.angle',
-    color: 'light.color',
-    'ground-color': 'light.groundColor',
-    decay: 'light.decay',
-    distance: 'light.distance',
-    intensity: 'light.intensity',
-    penumbra: 'light.penumbra',
-    type: 'light.type',
-    target: 'light.target'
+   mappings: {
+     angle: 'light.angle',
+     color: 'light.color',
+     'ground-color': 'light.groundColor',
+     decay: 'light.decay',
+     distance: 'light.distance',
+     intensity: 'light.intensity',
+     penumbra: 'light.penumbra',
+     type: 'light.type',
+     target: 'light.target',
+     'cast-shadow': 'light.castShadow',
+     'shadow-bias': 'light.shadowBias',
+     'shadow-camera-far': 'light.shadowCameraFar',
+     'shadow-camera-fov': 'light.shadowCameraFov',
+     'shadow-camera-near': 'light.shadowCameraNear',
+     'shadow-camera-top': 'light.shadowCameraTop',
+     'shadow-camera-right': 'light.shadowCameraRight',
+     'shadow-camera-bottom': 'light.shadowCameraBottom',
+     'shadow-camera-left': 'light.shadowCameraLeft',
+     'shadow-camera-visible': 'light.shadowCameraVisible',
+     'shadow-map-height': 'light.shadowMapHeight',
+     'shadow-map-width': 'light.shadowMapWidth'
   }
 });
